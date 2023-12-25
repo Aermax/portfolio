10c91fecdec10b70a06478c5276f79d8758442bf
Sid Shelke <ausv2027@gmail.com>
2023-12-25 19:20:12 +0530

    smart hover

--- components/animBlob.tsx
+++ components/animBlob.tsx
@@ -5,6 +5,7 @@ import { cn } from "../lib/utils"
 //bg-gradient-to-r from-green-300 via-blue-500 to-purple-600
 
 import { motion } from "framer-motion"
+import { FaArrowUp } from "react-icons/fa"
 import { useEffect, useState } from "react"
 
 interface AnimatedBlobProps {
@@ -14,29 +15,24 @@ interface AnimatedBlobProps {
 export const AnimatedBlob = ({ className }: AnimatedBlobProps) => {
   const [position, setPosition] = useState({
     x: 0,
-    y: 0
+    y: 0,
+    interacting: false
   })
 
   useEffect(() => {
     const mouseMove = (e: any) => {
+      const interactable = e.target.closest(".interactable")
+      const interacting = interactable !== null
       setPosition({
         x: e.clientX - 12,
-        y: e.clientY - 12
+        y: e.clientY - 12,
+        interacting: interacting
       })
     }
 
-    // const scrollMove = (e: any) => {
-    //   setPosition({
-    //     x: e.pageX,
-    //     y: e.pageY
-    //   })
-    // }
-
     window.addEventListener("mousemove", mouseMove)
-    // window.addEventListener("scroll", scrollMove)
     console.log(position)
     return () => {
-      // removeEventListener("scroll", scrollMove)
       removeEventListener("mousemove", mouseMove)
     }
   }, [position])
@@ -48,7 +44,7 @@ export const AnimatedBlob = ({ className }: AnimatedBlobProps) => {
     default: {
       x: position.x,
       y: position.y,
-      opacity: 1,
+      scale: position.interacting ? 4 : 1,
       transition: {
         duration: 0.1
       }
@@ -65,11 +61,21 @@ export const AnimatedBlob = ({ className }: AnimatedBlobProps) => {
         animate="default"
         id="blob"
         className={cn(
-          "z-50 pointer-events-none fixed top-0 left-0 h-[24px] w-[24px] rounded-full bg-gradient-to-r from-orange-300 via-blue-500 to-blue-600",
+          "z-50 flex items-center justify-center  pointer-events-none fixed top-0 left-0 h-[24px] w-[24px] rounded-full  dark:bg-white bg-white",
           className
         )}
-      ></motion.div>
-      {/* <div className="h-full w-full absolute transform -translate-y-[100%] z-[-1] backdrop-blur-[150px]"></div> */}
+      >
+        <motion.div
+          id="trailer-icon"
+          className={cn(
+            "relative transform rotate-45 text-black",
+            position.interacting ? "opacity-1" : "opacity-0"
+          )}
+        >
+          <FaArrowUp />
+        </motion.div>
+      </motion.div>
+      {/* <div className="h-full w-full fixed transform -translate-y-[100%] z-[-1] backdrop-blur-[150px]"></div> */}
     </>
   )
 }
